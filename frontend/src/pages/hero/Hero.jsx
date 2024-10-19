34ad2aad9f5c777a0e340d3a83e21254cfccabe2
Bhargav <www.bhargabadhikary80@gmail.com>
2024-10-19 19:35:28 +0530

    changes

--- frontend/src/pages/hero/Hero.jsx
+++ frontend/src/pages/hero/Hero.jsx
@@ -18,7 +18,7 @@ const Hero = () => {
         </h1>
         <p>
           Experience the rich and delectable world of Haldiram's cuisine at your
-          fingertips with the Haldiram Food Delivery App! Indulge in the
+          fingertips with the Haldiram Food Delivery Website! Indulge in the
           authentic flavors of India's favorite snacks, sweets, and savories,
           all conveniently delivered to your doorstep.
         </p>
